6a504cf943354190b96737859cf4bbe82af81957
Bernardo Ruiz <bernardorf1@gmail.com>
2024-05-02 11:37:29 +0000

    Release Staging environment with 1 changes made by Bernardo Ruiz

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -6,7 +6,7 @@ export interface Props {
    * @format rich-text
    * @default Click here to tweak this text however you want.
    */
-  textoDescritivo: string;
+  textoDescritivo?: string;
   logo?: ImageWidget;
 }
 
@@ -16,13 +16,13 @@ export default function HeroFlats({
   logo,
 }: Props) {
   return (
-    <div className="flex h-screen">
+    <div class="flex h-screen">
       {/* Left column */}
-      <div className="flex w-1/2">
+      <div class="flex w-1/2">
         
         {/* First sub-column */}
-        <div className="flex flex-col w-1/12">
-          <div className="h-10 bg-gray-200">
+        <div class="flex flex-col w-1/12">
+          <div class="h-10 bg-gray-200">
             <Image
                 src="https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/8677/83072e06-26a9-482d-aed4-cff656e7bc21"
                 alt="Image"
@@ -32,10 +32,10 @@ export default function HeroFlats({
         </div>
         
         {/* Second sub-column */}
-        <div className="flex flex-col overflow-auto p-10">
+        <div class="flex flex-col overflow-auto p-10">
           
           {/* First division */}
-          <div className="flex flex-col pb-15">
+          <div class="flex flex-col pb-15">
             <Image 
               src={logo}
               alt="Your image" 
@@ -44,29 +44,33 @@ export default function HeroFlats({
           </div>
           
           {/* Second division */}
-          <div className="flex flex-col h-1/3 overflow-auto mb-4 overflow-y-auto max-h-60 p-2 w-[30vw] h-[70vw]">
+          <div class="flex flex-col h-1/3 overflow-auto mb-4 overflow-y-auto max-h-60 p-2 w-[30vw] h-[70vw]">
               <p>{textoDescritivo}</p>
           </div>
           
           {/* Third division (empty) */}
-          <div className="flex flex-col items-center justify-center">
+          <div class="flex flex-col items-center justify-center">
             <p>post</p>
           </div>
         </div>
       </div>
       
       {/* Right column */}
-      <div className="h-full w-1/2 overflow-auto">
+      <div class="h-full w-1/2 overflow-auto">
         {/* Scrollable list content goes here */}
-        <div class="flex overflow-x-scroll">
-          <div class="flex-none w-48 h-24 bg-gray-700 mr-1">Item 1</div>
-          <div class="flex-none w-48 h-24 bg-gray-700 mr-1">Item 2</div>
-          <div class="flex-none w-48 h-24 bg-gray-700 mr-1">Item 3</div>
-          <div class="flex-none w-48 h-24 bg-gray-700 mr-1">Item 4</div>
-          <div class="flex-none w-48 h-24 bg-gray-700 mr-1">Item 1</div>
-          <div class="flex-none w-48 h-24 bg-gray-700 mr-1">Item 2</div>
-          <div class="flex-none w-48 h-24 bg-gray-700 mr-1">Item 3</div>
-          <div class="flex-none w-48 h-24 bg-gray-700 mr-1">Item 4</div>
+        <div class="flex overflow-x-scroll h-full">
+          <div class="flex-none w-60 h-full bg-gray-700 mr-1">
+            <div class="absolute bg-blue-700 h-36 bottom-0">
+              <p class="-rotate-90 text-left">Item 1</p>
+            </div>
+          </div>
+          <div class="flex-none w-48 h-full bg-gray-700 mr-1">Item 2</div>
+          <div class="flex-none w-48 h-full bg-gray-700 mr-1">Item 3</div>
+          <div class="flex-none w-48 h-full bg-gray-700 mr-1">Item 4</div>
+          <div class="flex-none w-48 h-full bg-gray-700 mr-1">Item 1</div>
+          <div class="flex-none w-48 h-full bg-gray-700 mr-1">Item 2</div>
+          <div class="flex-none w-48 h-full bg-gray-700 mr-1">Item 3</div>
+          <div class="flex-none w-48 h-full bg-gray-700 mr-1">Item 4</div>
         </div>
       </div>
     </div>
